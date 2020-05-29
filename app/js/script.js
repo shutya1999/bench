3881d37644416a7adf63c1d8362c93d8bd78acff
Roma Ishytin <romaishytin@gmail.com>
2020-05-29 22:23:39 +0300

    + burger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,16 @@
 "use strict";
 
-$(function() {
-    $('.nav-menu [href]').each(function() {
-        if (this.href === window.location.href) {
-            $(this).addClass('active');
-        }
+$(document).ready(function () {
+    $('.header-burger').click(function (event) {
+        $('.header-burger,.nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $(function() {
+        $('.nav-menu [href]').each(function() {
+            if (this.href === window.location.href) {
+                $(this).addClass('active-menu');
+            }
+        });
     });
 });
-
-
