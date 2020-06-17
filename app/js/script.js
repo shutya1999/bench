a93ccc5545348a7395ee3a01d84d034a59d1c195
Roma Ishytin <romaishytin@gmail.com>
2020-06-17 15:17:38 +0300

    adaptive 30%

--- app/js/script.js
+++ app/js/script.js
@@ -16,51 +16,11 @@ $(document).ready(function () {
         });
     });
 
-    // $('#btn').on('click', function () {
-        $('.filter').change(function() {
-            const values = $('input:checked', this).get().map(n => n.dataset.filter);
-            // console.log(values);
-            $('.cart-product').each((i, n) => $(n).toggle(!values.length || values.includes(n.dataset.category)));
-        }).change();
-    // })
-
-    $('#btn').on('click', function () {
-    //
-    //     let priceString = [];
-    //     $('.price span').each(function () {
-    //         let test = $(this).text();
-    //         priceString.push(test);
-    //     });
-    //
-    //     let priceNumber = [];
-    //     for (let i=0; i<priceString.length; i++){
-    //         priceNumber.push(Number(priceString[i]));
-    //     }
-    //     let test1 = priceNumber.sort();
-    //     let test2 = priceNumber.reverse();
-    //     // console.log(test2);
-    //
-    //     let typeSort = ($('#sort').val());
-    //     if (typeSort == 'low-hight'){
-    //         alert('Зростання');
-    //
-    //     }if (typeSort == 'hight-low'){
-    //         alert('Спадання');
-    //     }
-    //
-    //     for (let i=0; i<priceString.length; i++){
-    //
-    //     }
-    //
-    //     let items = $('#good .cart-product');
-    //     let arItems = $.makeArray(items);
-    //     console.log(arItems);
-    //     arItems .sort(function(a, b) {
-    //         return $(a).data("sort") - $(b).data("sort")
-    //     });
-    //     $(arItems).appendTo("#list");
-    })
-
+    $('.filter').change(function() {
+        const values = $('input:checked', this).get().map(n => n.dataset.filter);
+        // console.log(values);
+        $('.cart-product').each((i, n) => $(n).toggle(!values.length || values.includes(n.dataset.category)));
+    }).change();
 
 
 });
@@ -68,52 +28,57 @@ $(document).ready(function () {
 window.onload = function () {
     let preloader = document.getElementById('preloader');
     preloader.style.display = 'none';
+
+
+    let pathname = window.location.pathname;
+    let title = pathname.split(/[/.html]+/)[1];
+    document.title = title.charAt(0).toUpperCase() + title.substr(1);
+
+
+    document.querySelector('#btn').onclick = typeSort;
+
+    function typeSort() {
+        let typeSort = document.querySelector('#sort-price').value;
+
+        if (typeSort === 'up'){
+            mySortUp();
+        } else {
+            mySortDown();
+        }
+    }
+
+    function mySortUp() {
+        let sort = document.querySelector('#good');
+        // console.log(sort);
+
+        for (let i = 0; i < sort.children.length; i++){
+            for (let j = i; j < sort.children.length; j++){
+                if (+sort.children[i].getAttribute('data-price') > +sort.children[j].getAttribute('data-price')){
+                    let replaceNode = sort.replaceChild(sort.children[j], sort.children[i]);
+                    insertAfter(replaceNode, sort.children[i]);
+                }
+            }
+        }
+    }
+
+    function mySortDown() {
+        let sort = document.querySelector('#good');
+        // console.log(sort);
+
+        for (let i = 0; i < sort.children.length; i++){
+            for (let j = i; j < sort.children.length; j++){
+                if (+sort.children[i].getAttribute('data-price') < +sort.children[j].getAttribute('data-price')){
+                    let replaceNode = sort.replaceChild(sort.children[j], sort.children[i]);
+                    insertAfter(replaceNode, sort.children[i]);
+                }
+            }
+        }
+    }
+
+    function insertAfter(elem, refElem) {
+        return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+    }
 };
-
-document.querySelector('#btn').onclick = typeSort;
-
-function typeSort() {
-    let typeSort = document.querySelector('#sort-price').value;
-
-    if (typeSort === 'up'){
-        mySortUp();
-    } else {
-        mySortDown();
-    }
-}
-
-function mySortUp() {
-    let sort = document.querySelector('#good');
-    // console.log(sort);
-
-    for (let i = 0; i < sort.children.length; i++){
-        for (let j = i; j < sort.children.length; j++){
-            if (+sort.children[i].getAttribute('data-price') > +sort.children[j].getAttribute('data-price')){
-                let replaceNode = sort.replaceChild(sort.children[j], sort.children[i]);
-                insertAfter(replaceNode, sort.children[i]);
-            }
-        }
-    }
-}
-
-function mySortDown() {
-    let sort = document.querySelector('#good');
-    // console.log(sort);
-
-    for (let i = 0; i < sort.children.length; i++){
-        for (let j = i; j < sort.children.length; j++){
-            if (+sort.children[i].getAttribute('data-price') < +sort.children[j].getAttribute('data-price')){
-                let replaceNode = sort.replaceChild(sort.children[j], sort.children[i]);
-                insertAfter(replaceNode, sort.children[i]);
-            }
-        }
-    }
-}
-
-function insertAfter(elem, refElem) {
-    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
-}
-
 
 
 
